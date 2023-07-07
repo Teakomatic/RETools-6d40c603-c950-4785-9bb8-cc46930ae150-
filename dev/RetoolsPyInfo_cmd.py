bf59c18301e6480cff73faf491067ec36723b068
Max Suica <max.suica@gmail.com>
2023-07-07 13:48:51 -0700

    Rename RetoolsPyInfo command

--- dev/RetoolsPyInfo_cmd.py
+++ dev/RetoolsPyInfo_cmd.py
@@ -4,26 +4,24 @@ Diagnostic information for the ReTools rhino plugin.
 This command prints the python path to the rhino command line.
 """
 
-
 import sys
 
 from __plugin__ import title as plugin_name, version
-import doctools
-import geometry
-import geometry.fit
-import geometry.point
-import geometry.sampler
-import geometry.line
+
+from command import SUCCESS
 
 
 def RunCommand(is_interactive):
-    """Print the python path."""
+    """Print Diagnostic Information for Retools."""
+
+    # Plugin Name & Version
     print("Plugin: {}".format(plugin_name))
     print("Version: {}".format(version))
     print("")
-    print("Python path for plugin:")
 
+    # Python Path
+    print("Python path for plugin:")
     for item in sys.path:
         print(item)
 
-    return 0
+    return SUCCESS
