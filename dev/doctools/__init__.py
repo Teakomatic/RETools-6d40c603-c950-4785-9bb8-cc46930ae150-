cdcb2908e0ee18c076b16f0ccbdb78efc2d4f5cb
Max Suica <max.suica@gmail.com>
2023-07-07 13:49:33 -0700

    Move and update doctools

---
+++ dev/doctools/__init__.py
@@ -0,0 +1,55 @@
+"""
+This module contains functions for interacting with Rhino's
+document and docobjects.
+"""
+
+from scriptcontext import doc
+from rhinoscriptsyntax import ObjectType
+
+# Rhino document object types
+POINT = 1
+CURVE = 4
+
+
+class DocData:
+    ...
+
+
+class DocObjects:
+    def __init__(self, items):
+        self.curves = []
+        self.points = []
+        self.others = []
+
+        for item in items:
+            type = ObjectType(item)
+            if type == CURVE:
+                self.curves.append(item)
+            elif type == POINT:
+                self.points.append(item)
+            else:
+                print("Warning: Selected nonstandard geometry")
+                self.others.append(item)
+
+    def __nonzero__(self):
+        return self.curves or self.points or self.others
+
+    def __iter__(self):
+        return iter(self.points + self.curves)
+
+    def delete(self):
+        for obj in self.points + self.curves + self.others:
+            doc.Objects.Delete(obj)
+
+    @classmethod
+    def GetSelected(cls):
+        return cls(doc.Objects.GetSelectedObjects(False, False))
+
+    @staticmethod
+    def DeselectAll():
+        doc.Objects.UnselectAll()
+
+
+AddCurve = doc.Objects.AddCurve  # Geometry.Curve -> DocObject<Curve>
+AddCircle = doc.Objects.AddCircle  # Geometry.Circle -> DocObject<Circle>
+AddLine = doc.Objects.AddLine  # Geometry.Line -> DocObject<Line>
