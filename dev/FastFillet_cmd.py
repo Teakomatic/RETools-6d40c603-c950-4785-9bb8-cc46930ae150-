0bc8f34a02c7c5aa7beba51e52c3bdac65c94a32
Max Suica <max.suica@gmail.com>
2023-07-10 08:28:13 -0700

    Adapt fast fillet code

--- dev/FastFillet_cmd.py
+++ dev/FastFillet_cmd.py
@@ -1,14 +1,27 @@
-from Rhino.Geometry.Curve import CreateFilletCurves as Fillet
-from dev.doctools import DocObjects
+"""
+This command will attemp to fillet the currently selected curve or pair of curves.
+
+The command queries a repo for radius information. 
+
+This command is intended to be used with a user facing radius controller interface.
+
+In debug mode, we mark the starting points that we give to the native fillet command.
+
+We fail soft for all errors, so that we do not interupt the repeat/* macro
+"""
+
+from Rhino.Geometry.Curve import CreateFilletCurves
+from Rhino.Geometry import Point3d
 from scriptcontext import doc
-from Rhino.Geometry import PolyCurve
-from rhinoscriptsyntax import ObjectType
+from Rhino.Collections import Point3dList
 
+from command import SUCCESS
+from doctools import DocObjects, AddPoint, AddLine
+from geometry import closest, ease
+from repo import radius_repo
 
-# Generate diagnostic geometry for easing step
-DEBUG = False
+DEBUG = True
 
-# Rhino fillet operation options
 FILLET_OPTIONS = {
     "join": True,
     "trim": True,
@@ -19,69 +32,49 @@ FILLET_OPTIONS = {
 }
 
 
-def closest(self, other):
-    return min(
-        [(a, b) for a in self for b in other],
-        key=lambda ab: ab[0].DistanceToSquared(ab[1]),
-    )
+def RunCommand(is_interactive):
+    # Fetch current radius
+    FILLET_OPTIONS["radius"] = radius_repo.get()
+    print("Current radius is {}".format(FILLET_OPTIONS["radius"]))
 
-
-def ease(self, amount=0.1):
-    """Pick points close but distinct from a curve's ends.
-
-    Split a multi-segment curve into segments.
-    Ease each end point 10% along its containing segment.
-
-    Args:
-        amount (float, optional): Easing amount. Defaults to 0.1.
-
-    Returns:
-        [Point]: A pair of eased end points.
-    """
-    p = PolyCurve()
-    p.Append(self)
-    p.RemoveNesting()
-    first_segment = p.SegmentCurve(0)
-    last_segment = p.SegmentCurve(p.SegmentCount - 1)
-    eased_first = first_segment.PointAtNormalizedLength(amount)
-    eased_last = last_segment.PointAtNormalizedLength(1 - amount)
-    return eased_first, eased_last
-
-
-def fast_fillet():
     # Get selected curve geometry
     curve_docobjects = DocObjects.GetSelected().curves
     curves = [item.Geometry for item in curve_docobjects]
     n = len(curves)
 
-    # Validate selection
+    # Fail soft for invalid selection
     if n not in {1, 2}:
         print("Please select one or two curves. {} selected.".format(n))
 
         # Deselect if 3+ objects selected
         if n > 2:
             doc.Objects.UnselectAll()
-        exit()
+
+        return SUCCESS
 
         # n = 1: Keep object selected in case self fillet fails
         # This allows user to select a second object on next attempt
 
-    # Perform a two curve fillet
+    # Setup a two curve fillet
     if len(curves) == 2:
-        fillet_input_A, fillet_input_B = curves
-        p, q = ease(fillet_input_A), ease(fillet_input_B)
-        p, q = closest(p, q)
+        crv_A, crv_B = curves
+        # find the closest pair of starting points
+        ps, qs = ease(crv_A), ease(crv_B)
+        p, q = closest(ps, qs)
 
-    # Perform a self fillet
+    # Setup a self fillet
     if len(curves) == 1:
-        fillet_input_A = fillet_input_B = curves[0]
-        p, q = ease(fillet_input_A)
+        crv_A = crv_B = curves[0]
+        p, q = ease(crv_A)
 
-    fillet = Fillet(fillet_input_A, p, fillet_input_B, q, **FILLET_OPTIONS)
+    # Perform fillet
+    fillet = CreateFilletCurves(crv_A, p, crv_B, q, **FILLET_OPTIONS)
 
     if DEBUG:
-        doc.Objects.AddPoints([p, q])
-        doc.Objects.AddLine(p, q)
+        # Mark fillet initial conditions
+        AddPoint(p)
+        AddPoint(q)
+        AddLine(p, q)
 
     # Fail soft if fillet failed
     if not fillet:
@@ -89,7 +82,7 @@ def fast_fillet():
         if len(curves) == 2:
             DocObjects.DeselectAll()
         print("Fillet Failed")
-        exit()
+        return SUCCESS
 
     # Add filleted geometry
     for curve in fillet:
@@ -99,6 +92,8 @@ def fast_fillet():
     for curve in curve_docobjects:
         doc.Objects.Delete(curve)
 
+    return SUCCESS
 
-def RunCommand(is_interactive):
-    fast_fillet()
+
+if __name__ == "__main__":
+    RunCommand(True)
