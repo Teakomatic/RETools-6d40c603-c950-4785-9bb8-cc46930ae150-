2d6023791a765705193b0c0b404b98cdc14092fa
Max Suica <max.suica@gmail.com>
2023-09-30 17:28:06 -0700

    * chore(log.py): reorganize and clean up log.py file * feat(log.py): add print+file sink as default log sink * chore(log.py): move timestamp format into file *chore(log.py): update log format * docs(log.py): add constraints and documentation for the log module

--- dev/services/log.py
+++ dev/services/log.py
@@ -1,53 +1,68 @@
 """
-/import/lib/log.py
+/dev/services/log.py
 
 This module provides a simple logging interface for the ReTools library.
+
+Constraints:
+- The logger accepts a single string.
+- We have two log levels: debug (D) and info (I).
+- Error messages are logged always.
 """
 
 from functools import partial
 from os.path import abspath
 import System
 
-from my_time import timestamp
+import my_time
 
-# The log format can contain the following placeholders: {level}, {timestamp}, {msg}
-FORMAT = "{level} {timestamp}: {msg}"
+LOG_FILE = "C:\Users\maxsu\OneDrive\Desktop\Max Work\Max Work\Logs\REToolsPy.log"
+LOG_LEVEL = "D"
+LOG_MESSAGE_FORMAT = "{timestamp}({level}) {msg}"
+TIMESTAMP_FORMAT = "yy.MM.dd H:mm:ss.fff"
 
-LOG_FILE = abspath("../logs/import.log")
+# Utility functions
+def tee(f, g):
+    """Return a function that applies f and g to its argument."""
+    def h(x):
+        return f(x), g(x)
+    return h
 
-# Log sinks
-def FILE_LOGGER(msg):
-    open(LOG_FILE, "a").write(msg + "\n")
-def PRINT_LOGGER(msg):
+def print_fn(msg):
+    """Wrap the print statement into a function."""
     print(msg)
-def CONSOLE_LOGGER(msg):
-    System.Console.WriteLine
-DEFAULT_LOGGER = PRINT_LOGGER
 
-# Level may be  INFO or DEBUG
-LEVEL = "INFO"
+
+def FILE_SINK(msg):
+    with open(LOG_FILE, "a") as file:
+        file.write(msg + "\n")
+PRINT_SINK = print_fn
+FILE_PRINT_SINK = tee(FILE_SINK, PRINT_SINK)
+CONSOLE_SINK = System.Console.WriteLine
+DEFAULT_SINK = FILE_PRINT_SINK
 
 # Main logging function
-def log(msg, level, format=FORMAT):
+def log(msg, msg_level):
     """Log a message to the logger."""
-    if level == "DEBUG" and LEVEL != "DEBUG":
-        return
-    formatted_msg = format.format(
-        level=level,
-        timestamp=timestamp(),
-        msg=msg
-    )
-    DEFAULT_LOGGER(formatted_msg)
+    if  LOG_LEVEL == "D" or msg_level != "D":
+        formatted_msg = LOG_MESSAGE_FORMAT.format(
+            level=msg_level,
+            timestamp=my_time.timestamp(TIMESTAMP_FORMAT),
+            msg=msg
+        )
+        DEFAULT_SINK(formatted_msg)
+
 
 # Interface functions
-info = partial(log, level="INFO")
-error = partial(log, level="ERROR")
-debug = partial(log, level="DEBUG")
+debug = partial(log, msg_level="D")
+info = partial(log, msg_level="I")
+error = partial(log, msg_level="E")
+
 
 # Test
 if __name__ == "__main__":
+    info("Log file: {}".format(LOG_FILE))
+    info("Log level: {}".format(LOG_LEVEL))
+    info("Timestamp format: {}".format(TIMESTAMP_FORMAT))
+    debug("This is a debug message.")
     info("This is an info message.")
     error("This is an error message.")
-
-    DEFAULT_LOGGER = FILE_LOGGER
-    info("This is an info message with a file logger.")
