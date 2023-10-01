3e28e96918d098c139e2aae036df67b9ccf40311
Max Suica <max.suica@gmail.com>
2023-09-30 17:36:15 -0700

    * chore(RetoolsPyInfo_cmd.py): reformat code for better readability * feat(RetoolsPyInfo_cmd.py): command names to diagnostic output * feat(RetoolsPyInfo_cmd.py): number commands and path entries

--- dev/RetoolsPyInfo_cmd.py
+++ dev/RetoolsPyInfo_cmd.py
@@ -4,6 +4,7 @@ Diagnostic information for the ReTools rhino plugin.
 This command prints the python path to the rhino command line.
 """
 
+import os
 import sys
 
 from __plugin__ import title as plugin_name, version
@@ -11,18 +12,25 @@ from __plugin__ import title as plugin_name, version
 from command import SUCCESS
 from services.log import info
 
+SCRIPT_PATH = os.path.abspath(__file__)
+COMMAND_DIRECTORY = os.path.dirname(SCRIPT_PATH)
+COMMAND_FILES = [f for f in os.listdir(COMMAND_DIRECTORY) if f.endswith('_cmd.py')]
+COMMAND_NAMES = [f[:-7] for f in COMMAND_FILES]
 
 def RunCommand(is_interactive):
     """Print Diagnostic Information for Retools."""
-
-    # Plugin Name & Version
+    info("Retools Python Diagnostic Information")
     info("Plugin: {}".format(plugin_name))
     info("Version: {}".format(version))
     info("")
 
-    # Python Path
-    info("Python path for plugin:")
-    for item in sys.path:
-        info(item)
+    info("Commands:")
+    for number, command in enumerate(COMMAND_NAMES, start=1):
+        info("{number}: {command}".format(number=number, command=command))
+    info("")
+
+    info("Python path:")
+    for number, item in enumerate(sys.path, start=1):
+        info("{number}: {item}".format(number=number, item=item))
 
     return SUCCESS
