aac04347dc15b1ddd4bf0a4aa059c84ff8052e62
Max Suica <max.suica@gmail.com>
2023-10-02 04:15:31 -0700

    feat(LoadRetools_cmd.py): Report error when toolbar not found refactor(LoadRetools_cmd.py): consolidate OS logic to global scope

--- dev/LoadRETools_cmd.py
+++ dev/LoadRETools_cmd.py
@@ -9,8 +9,10 @@ Effects:
 - Reports the RETools version.
 """
 
+import os
+
 from __plugin__ import version
-from services.log import info, debug
+from services.log import info, debug, error
 from services.conf import OS
 from services.conf import TOOLBAR_PLIST_PATH
 from services.conf import TOOLBAR_RUI_PATH
@@ -18,19 +20,23 @@ from services.conf import TOOLBAR_RUI_PATH
 if OS == "Mac":
     from RhinoMac.Runtime import MacPlatformService
 
+    TOOLBAR_PATH = TOOLBAR_PLIST_PATH
+    is_toolbar_loaded = MacPlatformService.IsToolPaletteCollectionOpen
+    load_toolbar = MacPlatformService.LoadToolPaletteCollection
+
 else:
     from Rhino.RhinoApp import ToolbarFiles
-
-
+    
+    TOOLBAR_PATH = TOOLBAR_RUI_PATH
+    is_toolbar_loaded = ToolbarFiles.FindByPath
+    load_toolbar = ToolbarFiles.Open
+    
 def RunCommand(is_interactive):
-    if OS == "Mac":
-        if not MacPlatformService.IsToolPaletteCollectionOpen(TOOLBAR_PLIST_PATH):
-            debug("Loading RETools toolbar from {}".format(TOOLBAR_PLIST_PATH))
-            MacPlatformService.LoadToolPaletteCollection(TOOLBAR_PLIST_PATH)
-
+    if os.path.exists(TOOLBAR_PATH):
+        if not is_toolbar_loaded(TOOLBAR_PATH):
+            debug("Loading RETools toolbar from {}".format(TOOLBAR_PATH))
+            load_toolbar(TOOLBAR_PATH)
     else:
-        if not ToolbarFiles.FindByPath(TOOLBAR_RUI_PATH):
-            debug("Loading RETools toolbar from {}".format(TOOLBAR_RUI_PATH))
-            ToolbarFiles.Open(TOOLBAR_RUI_PATH)
+        error("RETools toolbar not found at {}. Skipping.".format(TOOLBAR_PATH)
 
-    info("Loaded ReTools Version {}.".format(version))
+    info("Loaded RETools Version {}.".format(version))
