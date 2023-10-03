42c9bd7b1551e0404607b93cbecbc45439c76db8
Max Suica <max.suica@gmail.com>
2023-10-03 05:47:25 -0700

    Merge FastFilletOptional into FastFillet

--- dev/FastFillet_cmd.py
+++ dev/FastFillet_cmd.py
@@ -10,15 +10,16 @@ In debug mode, we mark the starting points that we give to the native fillet com
 We fail soft for all errors, so that we do not interupt the repeat/* macro
 """
 
+import math
+
 from Rhino.Geometry.Curve import CreateFilletCurves
 from Rhino.Geometry import Point3d
 from scriptcontext import doc
 from Rhino.Collections import Point3dList
 
-from services.log import info
-
 from command import SUCCESS
-from dev.services.doc_objects import DocObjects, AddPoint, AddLine
+from services.doc_objects import DocObjects, AddPoint, AddLine
+from services.log import info, error, debug
 from geometry import closest, ease
 from repo import radius_repo
 
@@ -33,17 +34,57 @@ FILLET_OPTIONS = {
 }
 
 
-def RunCommand(is_interactive):
-    # Fetch current radius
-    FILLET_OPTIONS["radius"] = radius_repo.get()
-    info("Current radius is {}".format(FILLET_OPTIONS["radius"]))
+class InvalidSelection(Exception):
+    """
+    Exception for invalid selection
 
-    # Get selected curve geometry
-    curve_docobjects = DocObjects.GetSelected().curves
-    curves = [item.Geometry for item in curve_docobjects]
+    This means:
+    - No objects selected
+    - More than two objects selected   
+    """
+    pass
+
+
+class FilletFailure(Exception):
+    """
+    Exception for fillet failure
+
+    This means:
+    - Self fillet failed
+        - The curve is a line
+        - The curve is closed
+        - The ends are coplanar
+    - Two curve fillet failed
+        - The fillet radius is too large
+        - The fillet starting points diverge
+    """
+    pass
+
+
+class FailSoft(Exception):
+    """
+    Exception for soft failure
+    
+    This exception is used to indicate that the operation failed, but that
+    the command should not be aborted. This is used to allow the repeat
+    command to continue to the next iteration.
+    """
+    pass
+
+
+def select_curves():
+    """
+    Select the curves to fillet
+
+    Returns:
+        tuple[list[curve], int]: The selected curves and the number of curves
+    """
+    # User Prompt for Curve Selection (Currently automatic)
+    doc_curves = DocObjects.GetSelected().curves
+    curves = [item.Geometry for item in doc_curves]
     n = len(curves)
 
-    # Fail soft for invalid selection
+    # Selection Validation
     if n not in {1, 2}:
         info("Please select one or two curves. {} selected.".format(n))
 
@@ -51,50 +92,127 @@ def RunCommand(is_interactive):
         if n > 2:
             doc.Objects.UnselectAll()
 
-        return SUCCESS
-
         # n = 1: Keep object selected in case self fillet fails
         # This allows user to select a second object on next attempt
 
-    # Setup a two curve fillet
-    if len(curves) == 2:
-        crv_A, crv_B = curves
-        # find the closest pair of starting points
-        ps, qs = ease(crv_A), ease(crv_B)
-        p, q = closest(ps, qs)
+        raise FailSoft(InvalidSelection)
 
-    # Setup a self fillet
-    if len(curves) == 1:
+    return curves, doc_curves, n
+
+
+def get_radius():
+    """
+    Get the radius for the fillet operation
+
+    Returns:
+        float: The radius for the fillet operation
+    """
+    while True:
+        try:
+            # Radius Input Dialog
+#            rs.GetReal("Enter the fillet radius.", 0)
+            info("Enter the radius of the circle.")
+            radius_input = input("Radius: ")
+            debug("radius_input type: {}".format(type(radius_input)))
+
+            # Input Conversion
+            if type(radius_input) in {float, int}:
+                radius = radius_input
+
+            elif isinstance(radius_input, str):
+                radius = float(radius_input.strip())
+
+        # Input Validation
+        except ValueError:
+            error("Invalid input. Please enter a valid number.")
+            continue
+
+        except Exception as e:
+            error("Input Error: {}. Using default radius".format(e))
+            debug("Error type: {}".format(type(e)))
+            radius = 0
+
+        if radius < 0:
+            error("Radius must be a positive number or zero.")
+            continue
+
+        break
+
+    # Repository Radius Fetch (if applicable)
+    if radius == 0:
+        radius = radius_repo.get()
+
+    info("Current radius: {}".format(radius))
+    return radius
+
+
+def perform_fillet(curves, n, radius):
+    """
+    Perform the fillet operation
+
+    Args:
+        curves (list): The curves to fillet
+        n (int): The number of curves to fillet
+        radius (float): The radius of the fillet
+
+    Returns:
+        list[curve]: The fillet curves
+    """
+    # Fillet Parameter Configuration
+    if n == 1:
         crv_A = crv_B = curves[0]
         p, q = ease(crv_A)
 
-    # Perform fillet
+    elif n == 2:
+        crv_A, crv_B = curves
+        ps, qs = ease(crv_A), ease(crv_B)
+        p, q = closest(ps, qs)
+
+    FILLET_OPTIONS["radius"] = radius
+
+    # Fillet Execution
     fillet = CreateFilletCurves(crv_A, p, crv_B, q, **FILLET_OPTIONS)
 
+    # Parameter Tracing
     if DEBUG:
-        # Mark fillet initial conditions
         AddPoint(p)
         AddPoint(q)
         AddLine(p, q)
 
-    # Fail soft if fillet failed
+    # Result Validation
     if not fillet:
         # Deselect inputs if failed with two curves
-        if len(curves) == 2:
+        if n == 2:
             DocObjects.DeselectAll()
         info("Fillet Failed")
+
+        raise FailSoft(FilletFailure)
+
+    return fillet
+
+
+def RunCommand(is_interactive):
+    try:
+        # Command Initialization
+        curves, doc_curves, n = select_curves()  # FailSoft(InvalidSelection)
+        radius = get_radius()
+
+        # Fillet Execution
+        fillet_success = perform_fillet(curves, n, radius)  # FailSoft(FilletFailure)
+
+    # Failure Handling
+    except FailSoft as e:
+        type = e.args[0]
+        error("Operation soft failed with {}".format(type))
         return SUCCESS
+    
+    # Cleanup
+    DocObjects.DeleteObjects(doc_curves)
 
-    # Add filleted geometry
-    for curve in fillet:
-        doc.Objects.AddCurve(curve)
-
-    # Tidy old geometry
-    for curve in curve_docobjects:
-        doc.Objects.Delete(curve)
+    # Finalization
+    DocObjects.AddCurves(fillet_success)
 
     return SUCCESS
-
 
 if __name__ == "__main__":
     RunCommand(True)
