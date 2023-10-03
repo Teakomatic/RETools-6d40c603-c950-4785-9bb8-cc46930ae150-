96ed78c0fc0653758279fefda19be43ae48f6e95
Max Suica <max.suica@gmail.com>
2023-10-03 01:01:27 -0700

    * feat(ShowFolder_cmd.py): Support Multiplatform file explorers * feat: Report file directory to user * fix: Single quote folder name * refactor: Use service imports for logging and command vals

--- dev/ShowFolder_cmd.py
+++ dev/ShowFolder_cmd.py
@@ -4,21 +4,26 @@ This is a command template. It is used to create new commands.
 {command details here}
 """
 
-from command import SUCCESS, FAILURE
-from services.log import info, error
+
+from services import info, error, debug, SUCCESS, FAILURE
+from services.conf import OS
+from services.rhino import current_folder
+from services.process import spawn
+from services.my_string import singe_quote
+
+MAC_EXPLORER = "open"
+WIN_EXPLORER = "explorer"
+FILE_EXPLORER = MAC_EXPLORER if OS == "mac" else WIN_EXPLORER
 
 
 def RunCommand(is_interactive):
-    info("Running command template")
+    workdir = current_folder()
+    quoted_workdir = singe_quote(workdir)
+    info("Opening file explorer at {}".format(workdir))
+
+    spawn([
+        FILE_EXPLORER,
+        quoted_workdir,
+    ])
+
     return SUCCESS
-import process
-import rhino
-
-
-def main(workdir):
-    process.spawn("explorer " + workdir)
-
-
-if __name__ == "__main__":
-    workdir = rhino.current_folder()
-    main(workdir)
