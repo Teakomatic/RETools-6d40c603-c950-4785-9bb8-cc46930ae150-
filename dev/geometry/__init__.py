ed57d45dda11e87200e3c65a292c3137b063f54b
Max Suica <Max.Suica@gmail.com>
2024-07-17 07:59:26 -0700

    Move some basics into geometry

--- dev/geometry/__init__.py
+++ dev/geometry/__init__.py
@@ -3,3 +3,10 @@ from sample import sample
 from interpolate import interpolate
 from closest import closest
 from ease import ease
+
+
+from rhinoscriptsyntax import VectorAngle as angle
+from Rhino.Geometry.Vector3d import ZAxis as Z, CrossProduct as cross
+from rhinoscriptsyntax import XformRotation2 as rotate
+from Rhino.Geometry.Point3d import Origin
+from rhinoscriptsyntax import PlaneFitFromPoints as fit_plane
